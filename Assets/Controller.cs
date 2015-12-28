4e653abd29f12a3ebe3e0a2a7b45cc1a41a98d00
AleksandrSl <Alex121994@mail.ru>
2015-12-29 01:26:25 +0300

    First try

---
+++ Assets/Controller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller{
+	public static SingleMessage<Vector2> Rotate = new SingleMessage<Vector2>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
