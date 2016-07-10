a2f8fdbfdb69e46df1d9fc63d6d327cfa6b7dac5
AleksandrSl <Alex121994@mail.ru>
2016-07-10 12:04:18 +0300

    Change the position of function calls

--- Assets/Scripts/ExplosionSpawn.cs
+++ Assets/Scripts/ExplosionSpawn.cs
@@ -7,6 +7,8 @@ public class ExplosionSpawn:MonoBehaviour {
 	
     public void spawnExplosion(Vector2 collisionPoint)
     {
+        SoundManager.instance.PlaySingle(explSound);
+        GameObject explosion = Instantiate(explPrefab, collisionPoint, Quaternion.identity) as GameObject;
         Animator explosionAnim = explosion.GetComponent<Animator>();
         Controller.ExplSpawned.Say(explosionAnim.GetCurrentAnimatorStateInfo(0).length);
         DestroyObject(explosion, explosionAnim.GetCurrentAnimatorStateInfo(0).length);
