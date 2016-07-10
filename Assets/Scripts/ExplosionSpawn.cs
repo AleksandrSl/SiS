008bf8d15477c7b772b91d57b47c1b034da8e8bc
AleksandrSl <Alex121994@mail.ru>
2016-07-10 12:02:22 +0300

    Explosion sound added

--- Assets/Scripts/ExplosionSpawn.cs
+++ Assets/Scripts/ExplosionSpawn.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class ExplosionSpawn:MonoBehaviour {
-    public GameObject explosionPrefab;
+    public GameObject explPrefab;
+    public AudioClip explSound;
 	
     public void spawnExplosion(Vector2 collisionPoint)
     {
-        GameObject explosion = Instantiate(explosionPrefab, collisionPoint, Quaternion.identity) as GameObject;
         Animator explosionAnim = explosion.GetComponent<Animator>();
         Controller.ExplSpawned.Say(explosionAnim.GetCurrentAnimatorStateInfo(0).length);
         DestroyObject(explosion, explosionAnim.GetCurrentAnimatorStateInfo(0).length);
